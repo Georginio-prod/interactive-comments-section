e00efefbf0a22545d62ee214641a1c28acfd994e
Georginio-prod <etonameklou6@gmail.com>
2024-09-03 21:08:14 +0000

    style:position first card(end)

--- src/components/content.vue
+++ src/components/content.vue
@@ -5,42 +5,42 @@
             <div class="bg-white px-4 py-4">
                 <div class="flex ">
                     <img src="/public/avatars/image-amyrobson.png" alt="avatar" class="w-8 h-8">
-                    <p class="pl-4">
-                        amyrobson <span>1 month ago</span>
+                    <p class="pl-4 text-textcol ">
+                        amyrobson <span class="text-textdes pl-4">1 month ago</span>
                     </p>
                 </div>
-                <div class="">
+                <div class=" pt-4 text-textdes">
                     <p>
                         {{ msg }}
                     </p>
                     <div class="flex justify-between items-center">
-                        <div class="bg-bgcol flex">
-                            <p class="px-3 py-1">
-                                <button @click="conteur++" key="bA">+</button>
-                                {{ conteur }}
-                                <button @click="conteur--" key="bB">-</button>
+                        <div class="bg-bgcol flex rounded-xl mt-4">
+                            <p class="px-6 py-2">
+                                <button @click="conteur++" key="bA" class="pr-4 text-btn">+</button>
+                                <span class="text-num pr-4"> {{ conteur }}</span>
+                                <button @click="conteur--" key="bB" class=" text-btn">-</button>
                             </p>
                         </div>
-                        <div class="flex items-center">
+                        <div class="flex items-center mt-4">
                             <div v-if="!visible" class="flex items-center">
                                 <img src="/public/icon-reply.svg" alt="" class="mx-auto">
-                                <button @click="visible = true">
+                                <button @click="visible = true" class="text-num p-2">
                                     Reply
                                 </button>
                             </div>
                             <div v-else class="flex flex-col items-center">
                                 <div class="flex items-center">
                                     <img src="/public/icon-delete.svg" alt="">
-                                    <button>
+                                    <button class="p-2 text-sup">
                                         Delete
                                     </button>
-                                    <img src="/public/icon-edit.svg" alt="">
-                                    <button>
+                                    <img src="/public/icon-edit.svg" alt="" class="pl-3">
+                                    <button class="p-2 text-num ">
                                         Edit
                                     </button>
                                 </div>
 
-                                
+
                             </div>
 
                         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ export default {
     extend: {
       colors: {
         bgcol:"#F5F6FA",
+        textcol: "#334253",
+        textdes: '#67727E',
+        btn:'#C5C6EF',
+        num: '#5357B6',
+        sup: "#ED6368"
       }
     },
   },
